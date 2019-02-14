06292fe08e189767214c9b1e6297ccb4f210e09e
Matthew <matthewabebe.inbox@gmail.com>
2019-02-14 12:00:37 -0600

    login done

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -4,46 +4,46 @@ import runsCollection from "./runsCollection"
 const loginForm = {
     getAndAppendLogin() {
 
-let loginHeader = document.createElement("h2")
-loginHeader.textContent = "JogLog - Login"
+        let loginHeader = document.createElement("h2")
+        loginHeader.textContent = "JogLog - Login"
 
-let loginUserNameField = document.createElement("fieldset")
+        let loginUserNameField = document.createElement("fieldset")
 
-let loginUserNameLabel = document.createElement("label")
-loginUserNameLabel.textContent = "Username"
-loginUserNameLabel.setAttribute("for", "user_name")
-let loginUserNameInput = document.createElement("input")
-loginUserNameInput.setAttribute("id", "user_name")
-loginUserNameInput.setAttribute("name", "user_name")
+        let loginUserNameLabel = document.createElement("label")
+        loginUserNameLabel.textContent = "Username"
+        loginUserNameLabel.setAttribute("for", "user_name")
+        let loginUserNameInput = document.createElement("input")
+        loginUserNameInput.setAttribute("id", "user_name")
+        loginUserNameInput.setAttribute("name", "user_name")
 
-loginUserNameField.appendChild(loginUserNameLabel)
-loginUserNameField.appendChild(loginUserNameInput)
+        loginUserNameField.appendChild(loginUserNameLabel)
+        loginUserNameField.appendChild(loginUserNameInput)
 
-let loginUserEmailField = document.createElement("fieldset")
+        let loginUserEmailField = document.createElement("fieldset")
 
-let loginUserEmailLabel = document.createElement("label")
-loginUserEmailLabel.textContent = "Email Address"
-loginUserEmailLabel.setAttribute("for", "user_email")
-let loginUserEmailInput = document.createElement("input")
-loginUserEmailInput.setAttribute("id", "user_email")
-loginUserEmailInput.setAttribute("name", "user_email")
+        let loginUserEmailLabel = document.createElement("label")
+        loginUserEmailLabel.textContent = "Email Address"
+        loginUserEmailLabel.setAttribute("for", "user_email")
+        let loginUserEmailInput = document.createElement("input")
+        loginUserEmailInput.setAttribute("id", "user_email")
+        loginUserEmailInput.setAttribute("name", "user_email")
 
-loginUserEmailField.appendChild(loginUserEmailLabel)
-loginUserEmailField.appendChild(loginUserEmailInput)
+        loginUserEmailField.appendChild(loginUserEmailLabel)
+        loginUserEmailField.appendChild(loginUserEmailInput)
 
-let loginButton = document.createElement("button")
-loginButton.textContent = "Login"
-loginButton.setAttribute("class", "login_button")
-loginButton.addEventListener("click", this.handleLoginUser)
+        let loginButton = document.createElement("button")
+        loginButton.textContent = "Login"
+        loginButton.setAttribute("class", "login_button")
+        loginButton.addEventListener("click", this.handleLoginUser)
 
-let loginFormFragment = document.createDocumentFragment()
-loginFormFragment.appendChild(loginHeader)
-loginFormFragment.appendChild(loginUserNameField)
-loginFormFragment.appendChild(loginUserEmailField)
-loginFormFragment.appendChild(loginButton)
+        let loginFormFragment = document.createDocumentFragment()
+        loginFormFragment.appendChild(loginHeader)
+        loginFormFragment.appendChild(loginUserNameField)
+        loginFormFragment.appendChild(loginUserEmailField)
+        loginFormFragment.appendChild(loginButton)
 
-let loginFormArticle = document.querySelector(".output_loginform")
-loginFormArticle.appendChild(loginFormFragment)
+        let loginFormArticle = document.querySelector(".output_loginform")
+        loginFormArticle.appendChild(loginFormFragment)
     },
 
     handleLoginUser(event) {
@@ -56,14 +56,23 @@ loginFormArticle.appendChild(loginFormFragment)
             email: loginUserEmail
         }
 
-        runsCollection.getAllUsers()
-        .then(Users => {
-            console.log(Users)
-        const foundUser = Users.find(User => {
-            return (loginUser.name === User.name && loginUser.email === User.email)
-        })
-        alert("Welcome" + " " + foundUser.name)
-        sessionStorage.setItem("Id", foundUser.id)
-})}}
 
-export default loginForm;
+
+        runsCollection.getAllUsers()
+            .then(Users => {
+                let findUser = 1
+                Users.forEach(User => {
+                    if (loginUser.name === User.name && loginUser.email === User.email) {
+                    alert("Welcome" + " " + User.name)
+                    }
+                    else if
+                    (findUser === Users.length) {
+                        alert("User not found.")
+                    }
+
+                    else {
+                        findUser++
+
+                    }})})
+    }}
+                    export default loginForm;
